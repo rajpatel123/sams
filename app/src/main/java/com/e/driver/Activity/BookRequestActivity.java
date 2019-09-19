5293c26774249765cb1b34a1a0ac170a1d70438d
HP <shakya.nitin14@gmail.com>
2019-09-19 23:43:48 +0530

    nitin_BookRequestXml

---
+++ app/src/main/java/com/e/driver/Activity/BookRequestActivity.java
@@ -0,0 +1,15 @@
+package com.e.driver.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.e.driver.R;
+
+public class BookRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_request);
+    }
+}
