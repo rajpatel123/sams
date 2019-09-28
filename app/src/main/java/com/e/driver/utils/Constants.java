722bb9e53ecb5811bf63286ee4838ef7b3cef445
Lenovo <raj95288@gmail.com>
2019-09-28 23:38:53 +0530

    Umplemented Catlist and subcat list

--- app/src/main/java/com/e/driver/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.e.driver.utils;
-
-import android.content.Context;
-
-public class Constants {
-    public static final String LOGGED_IN ="loggedin" ;
-    public static String CAT_ID="cat_id";
-}
