f0bf000b9027420da4b1097223a42a5723ce66c1
raj <raj.bahadur@ziploan.biz>
2019-11-06 17:04:56 +0530

    check log after paayment

--- app/src/main/java/com/e/driver/interfaces/OnBookingClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.e.driver.interfaces;
-
-public interface OnBookingClickListener {
-    public void onBookingClick(int position);
-}
--- app/src/main/java/com/e/driver/models/paymentTransaction/ProxyCache.java
+++
@@ -1,8 +0,0 @@
-
-package com.e.driver.models.paymentTransaction;
-
-
-public class ProxyCache {
-
-
-}
